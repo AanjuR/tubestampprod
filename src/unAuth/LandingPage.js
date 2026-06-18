21829b07853212a94f13fc1dc31f89d95fcb78e1
Anju Singh <singh.anju29@gmail.com>
2026-06-18 15:49:28 -0700

    Landing paeg render

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,16 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <main className="landing-page__content">
+        <h1 className="landing-page__title">Hello Landing Page</h1>
+        <p className="landing-page__subtitle">
+          Welcome to TubeStamp.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default LandingPage;
